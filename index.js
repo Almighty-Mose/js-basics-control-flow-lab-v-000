fadb5ed5be41b4803b29ac727056b4ea64a2976d
Branden Mosley <branden.mosley1337@gmail.com>
2018-07-31 19:02:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code in this file!
+function scuberGreetingForFeet(num) {
+  let result
+  if (num < 400) {
+    result = "This one is on me!";
+  } else if (num > 2000 && num < 2500) {
+    result = "I will gladly take your thirty bucks.";
+  } else if (num > 2500) {
+    result = "No can do.";
+  }
+  return result
+}
+
+function ternaryCheckCity(city) {
+  city = "NYC" ? "OK, sounds good" : "No go."
+}
